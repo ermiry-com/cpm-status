185803a59dfb859ab5edb3be815f75444e83b4d2
ermiry <erick.salas@ermiry.com>
2021-05-02 23:58:16 -0500

    Version 0.1

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 #ifndef _CPM_VERSION_H_
 #define _CPM_VERSION_H_
 
-#define CPM_VERSION				"0.1"
+#define CPM_VERSION					"0.1"
 #define CPM_VERSION_NAME			"Version 0.1"
-#define CPM_VERSION_DATE			"06/04/2021"
-#define CPM_VERSION_TIME			"21:24 CST"
+#define CPM_VERSION_DATE			"02/05/2021"
+#define CPM_VERSION_TIME			"23:58 CST"
 #define CPM_VERSION_AUTHOR			"Erick Salas"
 
 // print full cpm version information
